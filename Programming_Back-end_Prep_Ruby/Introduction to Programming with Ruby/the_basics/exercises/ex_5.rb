07c8ce37611535ce5ca85e873462976bb68bbac2
Austin Okechukwu <austinokechukwu1@gmail.com>
2023-06-22 00:01:31 +0100

    Updating the README

---
+++ Programming_Back-end_Prep_Ruby/Introduction to Programming with Ruby/the_basics/exercises/ex_5.rb
@@ -0,0 +1,15 @@
+# Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+
+
+factioral_5 = 5 * 4 * 3 * 2 * 1
+puts factioral_5
+
+factioral_6 = 6 * 5 * 4 * 3 * 2 * 1
+puts factioral_6
+
+
+factioral_7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts factioral_7
+
+factioral_8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts factioral_8
