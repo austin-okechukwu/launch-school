3ee867ab839ff5921c9b14fb320f930a9d6b524d
Austin Okechukwu <austinokechukwu1@gmail.com>
2023-06-21 23:21:01 +0100

    Completed exercises for the_basics from Introduction to Programming with ruby book

---
+++ Programming & Back-end Prep Ruby/Introduction to Programming with Ruby/the_basics/exercises/ex_1.rb
@@ -0,0 +1,7 @@
+# Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+
+# Copy Code
+# "<Firstname> <Lastname>"
+# For example, if your name is John Doe, think about how you can put "John" and "Doe" together to get "John Doe".
+
+puts "Austin " + "Okechukwu"
---
+++ Programming & Back-end Prep Ruby/Introduction to Programming with Ruby/the_basics/exercises/ex_4.rb
@@ -0,0 +1,15 @@
+# Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+# 1975
+# 2004
+# 2013
+# 2001
+# 1981
+
+
+years_list = [1975, 2004, 2013, 2001, 1981]
+
+puts years_list[0]
+puts years_list[1]
+puts years_list[2]
+puts years_list[3]
+puts years_list[4]
