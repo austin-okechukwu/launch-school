7bd6c77198ea73b66b9275c0ce5ca88718fd52b0
Austin Okechukwu <austinokechukwu1@gmail.com>
2023-06-22 00:08:23 +0100

    Updated README and file structure

--- Programming & Back-end Prep Ruby/Introduction to Programming with Ruby/the_basics/exercises/ex_6.rb
+++
@@ -1,6 +0,0 @@
-# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
-
-
-puts 3.01 ** 2
-puts 4.001 ** 2
-puts 5.0001 ** 2
--- Programming & Back-end Prep Ruby/Introduction to Programming with Ruby/the_basics/exercises/ex_7.rb
+++
@@ -1,7 +0,0 @@
-# What does the following error message tell you?
-
-# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
-#   from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
-
-# The error shows that instead of a closing curly bracket denoted '}' it shows a normal clsoed brackett suggesting user 
-# Might have inputted the wrong character.
