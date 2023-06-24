534853d7a7e808c95b0d9f13c5828d61aa5ff786
Austin Okechukwu <austinokechukwu1@gmail.com>
2023-06-24 15:17:41 +0100

    Finished with variable methodss chapter

---
+++ Programming_Back-end_Prep_Ruby/Introduction_to_Programming_with_Ruby/methods/exercises/ex_3.rb
@@ -0,0 +1,8 @@
+# Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def multiply(a,b)
+  product = a*b
+  return product
+end
+
+puts multiply(5,6)
